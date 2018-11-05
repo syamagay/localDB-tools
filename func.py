ba64288968ad4445a0ca95ef1a39cad135311032
ArisaKubota <arisa.kubota@cern.ch>
2018-11-05 14:45:57 +0900

    modified master

--- func.py
+++ func.py
@@ -5,14 +5,18 @@ def setTime(date):
     time = (date+datetime.timedelta(hours=DIFF_FROM_UTC)).strftime("%Y/%m/%d %H:%M:%S")
     return time
 
-def readJson(fileName):
-    f = open(fileName, 'r')
-    json_data = json.load(f)
-    f.close()  
-    return json_data
 
 def writeJson(fileName, data):
     f = open(fileName, 'w')
     json.dump(data,f,indent=4)
     f.close()        
-    
+ 
+def readJson(fileName):
+    try:
+        f = open(fileName, 'r')
+    except:
+        f = open("parameter_default.json", 'r')
+    json_data = json.load(f)
+    f.close()  
+    return json_data
+   
