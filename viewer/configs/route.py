ce51719a9dd869619662dcce98ab46fe9b9b1b6d
Eunchong Kim <eunchong.kim@cern.ch>
2019-08-03 16:19:22 +0900

    Moved controller to pages in viewer

--- viewer/configs/route.py
+++ viewer/configs/route.py
@@ -6,7 +6,9 @@ from functions.logging import *
 #==============================
 # Pages and blue prints
 #==============================
-from pages.sync_statistics import sync_statistics_api
+from pages.callback         import callback_api
+from pages.component_dev    import component_dev_api
+from pages.sync_statistics  import sync_statistics_api
 
 #==============================
 # Scripts, Will be replaced by ?
@@ -14,9 +16,3 @@ from pages.sync_statistics import sync_statistics_api
 from scripts.src      import listset
 from scripts.src      import static
 from scripts.src.func import *
-
-#==============================
-# Controllers Blue Prints
-#==============================
-from controllers.callback   import callback_api
-from controllers.component_dev   import component_dev_api
